df5a26fc2ee2d75059c9aee9a1b3c97b353b6937
Aiden Tjahjono <68307838+joker69bot@users.noreply.github.com>
2022-12-10 10:48:06 -0800

    Upload Website

---
+++ app.js
@@ -0,0 +1,22 @@
+window.onscroll = function() {
+    myFunction()
+}
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky");
+    } else {
+        navbar.classList.remove("sticky");
+    }
+}
+
+function highlight(x) {
+    x.style.background = "yellow";
+}
+
+function dehighlight(x) {
+    x.style.background = "";
+}
